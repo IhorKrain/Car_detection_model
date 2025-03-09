798bbf6616748e37cdb39883222eb9dbfd008766
igork <Kraynik.igor15@gmail.com>
2025-03-09 19:18:40 +0100

    rename file

--- car_detection_model/models_predict_vizualize.py
+++ car_detection_model/models_predict_vizualize.py
@@ -4,12 +4,11 @@ from matplotlib import pyplot as plt
 model1 = YOLO("C:/Users/Public/pycharm/Yolo_detection/car_detection_model/best.pt")
 model2 = YOLO("C:/Users/Public/pycharm/Yolo_detection/car_detection_model/bests.pt")
 def detect_image(image_path,model):
-    # Предсказание
     results = model.predict(image_path, conf=0.5, save=True)
 
-    # Визуализация с Matplotlib
+    #  Vizualizing
     for r in results:
-        im_array = r.plot()  # Рисуем bounding boxes
+        im_array = r.plot()
         plt.figure(figsize=(10, 10))
         plt.imshow(im_array[..., ::-1])  # BGR -> RGB
         plt.axis('off')
